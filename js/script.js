e18c5571c0d98ed4a747853b1ef2ca08615711dc
Alexander Urlich <ulrichalex12@yahoo.de>
2021-09-26 22:23:43 +0200

    Template for pokemonlist and Objects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+let pokemonlist = [];
+
+let pokemon1 = {
+    name: '',
+    type: '',
+    height: []
+
+}
+
+pokemonlist.push(pokemon1);
+
+let pokemon1 = {
+    name: '',
+    type: '',
+    height: []
+
+}
+
+pokemonlist.push(pokemon1);
+
+
+let pokemon1 = {
+    name: '',
+    type: '',
+    height: []
+
+}
+
+pokemonlist.push(pokemon1);
