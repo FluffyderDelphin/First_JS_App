aa1ec085da8c7a12d472f840e3527357c8b452db
Alexander Urlich <ulrichalex12@yahoo.de>
2021-10-01 18:38:49 +0200

    added validation checks for the pokemon reposetory

--- js/script.js
+++ js/script.js
@@ -20,12 +20,54 @@ let pokemon3 = {
     height: 1.5
 
 }
+
+//  Functions Checks if Pokemon Object is valid
+let checkObject = function (pkobj) {
+    if (typeof pkobj === 'object'
+        && Object.keys(pkobj).includes('name')
+        && Object.keys(pkobj).includes('type')
+        && Object.keys(pkobj).includes('height')
+    ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//   Function to that adds a Comments based on the Pokemons height
+
+let checkheight = (height) => {
+    if (typeof height === 'number') {
+        return height >= 1.6 ? 'Wow thats big! ' : ' ';
+    } else {
+        return false;
+    }
+}
+
+
+//   Function to that adds a Comments based on the Pokemons type
+let checktype = function (type) {
+    if (type.length === 2) {
+        return 'It has two Types !';
+    } else if (type.length === 1) {
+        return 'It has one Type !';
+    } else {
+        // In case of any Invalid "Type" Values 
+        return 'A Pokemon can only have 1 or 2 Types !';
+    }
+}
+
+
+
 let pokemon_reposetory = (function () {
     let pokemonlist = [];
 
     return {
         add: function (pokemon) {
-            if (typeof pokemon === 'object' && Object.keys(pokemon) === ['name', 'type', 'height']) {
+            if (checkObject(pokemon)
+                && checktype(pokemon.type)
+                && checkheight(pokemon.height)) {
                 pokemonlist.push(pokemon)
             } else {
                 console.log('Invalid Input')
@@ -41,22 +83,8 @@ let pokemon_reposetory = (function () {
 pokemon_reposetory.add(pokemon1);
 pokemon_reposetory.add(pokemon2);
 pokemon_reposetory.add(pokemon3);
+pokemon_reposetory.add({ name: 'Pikachu', type: ['electro'], height: 0.5 });
 
-//   Function to that adds a Comments based on the Pokemons height
-let checkheight = (height) => height >= 1.6 ? 'Wow thats big! ' : '';
-
-
-//   Function to that adds a Comments based on the Pokemons type
-let checktype = function (type) {
-    if (type.length === 2) {
-        return 'It has two Types !';
-    } else if (type.length === 1) {
-        return 'It has one Type !';
-    } else {
-        // In case of any Invalid "Type" Values 
-        return 'A Pokemon can only have 1 or 2 Types !'
-    }
-}
 
 // Looping trough the Pokemonlist. It will write the Name and  the height of the Pokemons, while also Comment on the height via the function and also how many Types it has
 
@@ -64,10 +92,8 @@ let checktype = function (type) {
 pokemon_reposetory.getALL().forEach((property) => {
     console.log(property.name)
     document.write(`That Pokemon´s Name is ${property.name}! 
-    Its height is ${property.height}.
+    Its height is ${property.height} m.
     ${checkheight(property.height)} 
     ${checktype(property.type)} <br>`)
 });
 
-
-console.log(Object.keys(pokemon1));
